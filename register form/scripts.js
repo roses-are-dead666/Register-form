eb131baf89567088083bf669bbe6dcb8a2cb31f9
Arzhan Bakytbek <79097350+roses-are-dead666@users.noreply.github.com>
2021-04-11 18:09:16 +0600

    Add files via upload

---
+++ register form/scripts.js
@@ -0,0 +1,12 @@
+$('.box__login').on('click', function(e){
+    e.preventDefault();
+    $(this).toggleClass('boxLoginBorder');
+});
+
+
+$('i').on('click', function(e){
+    e.preventDefault();
+    $(this).toggleClass('boxLoginBorder');
+});
+
+
